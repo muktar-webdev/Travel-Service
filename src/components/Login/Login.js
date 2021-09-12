141a7d80e59a3d8ce917716809fa2ea83498f7f2
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-12 22:51:06 +0600

    Login Form Conditional Rendering

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,48 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 // --Icon Images --//
 import Google from "../../images/googleicon2.png";
 import Facebook from "../../images/Facebook-logo.png";
 
 const Login = () => {
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  // --HandleUserInput--//
+  const handleUserInput = (e) => {
+    const { name, value } = e.target;
+
+    //--Form Validation --//
+    let isFieldValid = true;
+    if (name === "email") {
+      isFieldValid = /\S+@\S+\.\S+/.test(value);
+    }
+    if (name === "password") {
+      isFieldValid = value.length > 6 && /\d/.test(value);
+    }
+    //--Sent to State (user state) --//
+    if (isFieldValid) {
+      setUser((preValue) => {
+        return {
+          ...preValue,
+          [name]: value,
+        };
+      });
+    }
+  };
   return (
     <div className="login-container">
       {/* -Login form-- */}
       <div className="form-container">
         <form>
-          <h2>Login</h2>
+          {newUser ? (
+            <h2 style={{ color: "grey" }}>Create an Account</h2>
+          ) : (
+            <h2 style={{ color: "grey" }}>Login</h2>
+          )}
           <br />
-          <label htmlFor="">Name</label>
-          <br />
-          <br />
-          <input type="text" name="name" value="" />
-          <br />
+          {newUser && (
+            <div>
+              <label htmlFor="name">Name</label>
+              <br />
+              <br />
+              {/* --Name Field-- */}
+              <input type="text" name="name" onBlur={handleUserInput} />
+            </div>
+          )}
           <br />
           <label htmlFor="email">Email</label>
           <br />
           <br />
-          <input type="email" name="email" value="" />
+          {/* --Email Field-- */}
+          <input type="email" name="email" onBlur={handleUserInput} required />
           <br />
           <label htmlFor="password">Password</label>
           <br />
           <br />
-          <input type="text" name="password" value="" />
+          {/* --password field-- */}
+          <input
+            type="password"
+            name="password"
+            onBlur={handleUserInput}
+            required
+          />
           <br />
           <br />
+          {/* --Submit Field-- */}
           <input
             type="submit"
-            value="Login"
+            value={newUser ? "Register" : "Login"}
             style={{
               backgroundColor: "#ff5959",
               color: "white",
               padding: "0.6rem",
               fontSize: "1rem",
             }}
-          />{" "}
+          />
           <br />
           <p style={{ marginTop: "1rem", textAlign: "center" }}>
-            Don't have an account ?{" "}
-            <span style={{ color: "orange" }}>Create an Account</span>
+            {newUser ? null : "Don't have an account ? "}
+            <span
+              style={{ color: "orange", cursor: "pointer" }}
+              onClick={() => setNewUser(!newUser)}
+            >
+              {newUser ? null : "Create an Account"}
+            </span>
           </p>
         </form>
 
